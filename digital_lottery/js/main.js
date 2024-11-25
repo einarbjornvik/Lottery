da081100891bf441a2bb1c60805b5ddfbfcc0c84
einarbjornvik <einaar.bjornvik@gmail.com>
2024-11-25 09:56:34 +0100

    Beta

--- digital_lottery/js/main.js
+++ digital_lottery/js/main.js
@@ -12,10 +12,9 @@ const btnSaveSettings = document.querySelector("#save_settings");
 
 loadEventListeners();
 function loadEventListeners(){
-    console.log("event listener OK");
+    console.log("event listener running");
 
     btnAdd.addEventListener("click", addName);
-
     btnSaveSettings.addEventListener("click", saveSettings);
 
     document.addEventListener("DOMContentLoaded", showParticipants);
@@ -27,11 +26,11 @@ function loadEventListeners(){
 function addName(e)
 {
     e.preventDefault();
-    console.log("add name OK");
+    console.log("addName running");
     let paidEnough;
     let priceOneTicket;
     let allNames;
-
+    let allTickets;
 
     if (localStorage.getItem("ticketprice") === null)
     {
@@ -43,25 +42,21 @@ function addName(e)
     }
 
 
-
     if (Math.floor(paid.value / 10) >= 1)
     {
         console.log("betalt nok");
-
         paidEnough = true;
     }
     else
     {
         console.log("ikke betalt nok");
-
         paidEnough = false;
     }
 
 
-
     if (paidEnough === true)
     {
-        if (localStorage.getItem("savedNames") === null) //få verdier fra local storage
+        if (localStorage.getItem("savedNames") === null) 
         {
             allNames = [];
         }
@@ -71,41 +66,69 @@ function addName(e)
         }
 
 
-
         if (nameInput.value != "") //om navn ikke er tomt
-            {
-                allNames.push(nameInput.value);
-                allNames.sort();
-                
-            }
+        {
+            allNames.push(nameInput.value);
+            allNames.sort();
+        }
         else
-            {
-                alert("navn må fylles ut!");
-            }
+        {
+            alert("navn må fylles ut!");
+        }
 
-        localStorage.setItem("savedNames", JSON.stringify(allNames)); //skriv til minne
+        //skriv til minne
+        localStorage.setItem("savedNames", JSON.stringify(allNames));
+        
+
+        if (localStorage.getItem("savedTickets") === null)
+        {
+            allTickets = [];
+        }
+        else
+        {
+            allTickets = JSON.parse(localStorage.getItem("savedTickets"));
+        }
+
+
+        //hvor mange lodd til hver enkelt spiller
+        for (let i = 0; Math.floor(paid.value/priceOneTicket) > i; i++)
+        {
+            allTickets.push(nameInput.value)
+        }
+
+        //skriv til minne antall lodd for hvert navn
+        localStorage.setItem("savedTickets", JSON.stringify(allTickets)) 
     }
 
     nameInput.value = "";
     nameInput.focus();
 
     showParticipants();
+    showTickets();
+    
     console.log(allNames);
+}
 
+
+
+//tegn opp alle lodd
+function showTickets()
+{
+    console.log("showTickets running");
 }
 
 
 
 function showParticipants()
 {
+    console.log("showParticipants running");
     let savedNames;
-
-
 
     while(participants.firstChild)
     {
         participants.removeChild(participants.lastChild);
     }
+
 
     if (localStorage.getItem("savedNames") === null)
     {
@@ -116,8 +139,7 @@ function showParticipants()
         savedNames = JSON.parse(localStorage.getItem("savedNames"));
     }
 
-
-
+    
     savedNames.forEach((name) =>
     { 
         const li = document.createElement("li");
@@ -131,6 +153,8 @@ function showParticipants()
 
 function saveSettings() //lagre innstillinger
 {
+    console.log("saving settings");
+
     localStorage.setItem("ticketPrice", ticketPrice.value);
     localStorage.setItem("numberWinners", numberWinners.value);
 
@@ -139,6 +163,7 @@ function saveSettings() //lagre innstillinger
     console.log("antall vinnere:" + numberWinners.value);
     
 }
+
 function getSettings() //ta ut innstillinger fra lokalt minne
 {
     console.log("getting settings");
