ccffbe18461d0505e914f91d677a5243ed41599c
einarbjornvik <einaar.bjornvik@gmail.com>
2024-11-28 15:25:01 +0100

    beta

--- digital_lottery/js/main.js
+++ digital_lottery/js/main.js
@@ -8,19 +8,51 @@ const numberWinners = document.querySelector("#numberWinners");
 const btnSaveSettings = document.querySelector("#save_settings");
 const showTickets = document.querySelector(".tickets");
 
-
 //globale variabler
 // let allNames = ["einar", "terje"];
 
+
+
+function checkTicket(event)
+{
+    // console.log(event);
+
+    //lage variabler
+    let clickName;
+    let clickID;
+    const allTickets = document.querySelectorAll(".ticket");
+
+    //hvilket lodd ble klikket
+    console.log(event.target.parentElement.getAttribute("name"));
+
+    if(event.target.parentElement.classList.contains("ticket"))
+    {
+        clickName = event.target.parentElement.getAttribute("name");
+        clickID = evnet.target.parentElement.getAttribute("id");
+    }
+}
+
+
+
 loadEventListeners();
-function loadEventListeners(){
+function loadEventListeners()
+{
     console.log("event listener running");
 
+    //add navn med add-knappen
     btnAdd.addEventListener("click", addName);
+
+    //lagre innstillinger
     btnSaveSettings.addEventListener("click", saveSettings);
 
+    //tegn opp alle deltakere
     document.addEventListener("DOMContentLoaded", showParticipants);
+
+    //hent innstillinger
     document.addEventListener("DOMContentLoaded", getSettings);
+
+    //bruker klikker på lodd
+    showTickets.addEventListener("click", checkTicket);
 }
 
 
@@ -29,6 +61,8 @@ function addName(e)
 {
     e.preventDefault();
     console.log("addName running");
+
+    //lage variabler
     let paidEnough;
     let priceOneTicket;
     let allNames;
@@ -105,9 +139,13 @@ function addName(e)
     nameInput.value = "";
     nameInput.focus();
 
+    //skriver navn under participants
     showParticipants();
-    makeTickets(allTickets);
-    
+
+    //mikser loddene
+    mixTickets();
+
+
     // console.log(allNames);
 }
 
@@ -118,15 +156,31 @@ function makeTickets(everyTicket)
 {
     console.log("makeTickets running");
 
+    //opprydding i loddene, stopping av akkumuleringen
+    while(showTickets.firstChild)
+    {
+        showTickets.removeChild(showTickets.lastChild);
+    }
+
+    //lager en teller
+    let i = 1;
+
     //sjekk om det er lodd i minnet
     if (localStorage.getItem("savedTickets") !== null)
     {
         console.log(everyTicket);
         everyTicket.forEach(name => {
             const newTicket = document.createElement("div");
+
             newTicket.className = "ticket fresh";
-            newTicket.innerHTML = `<h2>${name}</h2>`;
+            newTicket.setAttribute("id", i);
+            newTicket.setAttribute("name", name);
+
+            newTicket.innerHTML = `<h2>${i}</h2><br><p>${name}</p>`;
+
             showTickets.appendChild(newTicket);
+
+            i++;
         })
     }
 }
@@ -137,6 +191,7 @@ function showParticipants()
 {
     console.log("showParticipants running");
     let savedNames;
+
 
     while(participants.firstChild)
     {
@@ -166,7 +221,8 @@ function showParticipants()
 
 
 
-function saveSettings() //lagre innstillinger
+//lagre innstillinger
+function saveSettings() 
 {
     console.log("saving settings");
 
@@ -176,10 +232,9 @@ function saveSettings() //lagre innstillinger
     console.log("saving settings");
     console.log("pris per lodd:" + ticketPrice.value);
     console.log("antall vinnere:" + numberWinners.value);
-    
 }
-
-function getSettings() //ta ut innstillinger fra lokalt minne
+//ta ut innstillinger fra lokalt minne
+function getSettings() 
 {
     console.log("getting settings");
     
@@ -189,4 +244,54 @@ function getSettings() //ta ut innstillinger fra lokalt minne
     
     const winnerAmount = localStorage.getItem("numberWinners");
     numberWinners.value = winnerAmount || 1;
+}
+
+
+
+//stokke om bilettene
+function shuffle(array)
+{
+    console.log("shuffling tickets");
+
+    //lage variabler
+    let currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+    //mens det er noe kan flyttes
+    while (0 !== currentIndex)
+    {
+        //velg et element som ikke tidligere er valgt
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        //bytt med denne indexen
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+//stokke om bilettene???
+function mixTickets()
+{
+    console.log("mixing tickets");
+
+    let allTickets;
+    if (localStorage.getItem("savedTickets") === null)
+    {
+        allTickets = [];
+    }
+    else
+    {
+        allTickets = JSON.parse(localStorage.getItem("savedTickets"));
+    }
+
+    //lag tilfeldig rekkefølge
+    shuffle(allTickets);
+
+    //flytter denne
+    makeTickets(allTickets);
 }
